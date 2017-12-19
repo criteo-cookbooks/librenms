17aedd9f9f3933a2cdd5a794e4957fb331c0c5f1
Nicolas Grieco <n.grieco@criteo.com>
2017-12-19 11:10:03 +0100

    Removing cookbook-release dependency

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'use_github_issues@criteo.com'
 license 'All rights reserved'
 description 'Installs/Configures librenms'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
+version '0.2.3'
 supports 'centos7'
 
 source_url 'https://github.com/criteo-cookbooks/librenms' if respond_to?(:source_url)
@@ -12,4 +12,3 @@ issues_url 'https://github.com/criteo-cookbooks/librenms/issues' if respond_to?(
 
 depends	'apache2'
 depends	'logrotate'
-depends 'cookbook-release'
