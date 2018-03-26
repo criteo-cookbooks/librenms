dad0995e733a7177a987af9fee52856cf31c1f82
Cedric Paillet <c.paillet@criteo.com>
2018-03-26 09:48:39 +0200

    Update rrdcache startup cmd

--- attributes/default.rb
+++ attributes/default.rb
@@ -80,8 +80,8 @@ default['librenms']['cron']['check']['minute'] = '*/5'
 # rrdcached
 default['librenms']['rrdcached']['enabled'] = false
 default['librenms']['rrdcached']['config_file'] = '/etc/sysconfig/rrdcached'
-default['librenms']['rrdcached']['options'] = '-w 1800 -z 1800 -f 3600 -B -R -j /var/tmp -l unix:/var/run/rrdcached/rrdcached.sock -t 4 -F'
-default['librenms']['rrdcached']['path'] = "node['librenms']['path']/rrd"
+default['librenms']['rrdcached']['options'] = '-w 1800 -z 1800 -f 3600 -j /var/tmp -l unix:/var/run/rrdcached/rrdcached.sock -t 4 -F'
+default['librenms']['rrdcached']['path'] = "node['librenms']['path']/rrd/"
 
 # autodiscover switches
 default['librenms']['autodiscover']['xdp'] = false
